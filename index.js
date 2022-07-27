fe7b910375534a8ebf7b25e0e9f7bcee09359d50
Fawwaz Firdaus <muhammadfawwaz463@gmail.com>
2022-07-27 22:04:52 +0700

    Menambajkan put dan delete pada io cors

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const httpServer = createServer(app)
 const io = new Server(httpServer, {
   cors: {
     origin: 'https://chatopia-fwzfrds.vercel.app',
-    methods: ['GET', 'POST']
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
   }
 })
 
